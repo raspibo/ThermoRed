dec6156867513d61930634c2b5f418fc06da4262
dave4th <mail4davide@gmail.com>
2014-10-26 18:40:15 +0100

    Set point settimanali

--- cgi-bin/readsetpoints.py
+++ cgi-bin/readsetpoints.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import os
+
 import json
 
 import cgi
@@ -7,8 +9,21 @@ import cgitb
 
 cgitb.enable()
 
-with open("setpointarray.json") as json_file:
-    SetPoints = json.load(json_file)
+# Se il file esiste lo apro, se no, genero entrambi SetPoints e file
+if os.path.exists("setpointarray.json"):
+	with open("setpointarray.json") as json_file:
+		SetPoints = json.load(json_file)
+else:
+	SetPoints = \
+		[	\
+		    { "display": "Temperatura Giorno", "name": "Tday", "temperature": "20" },	\
+		    { "display": "Temperatura Notte", "name": "Tnight", "temperature": "16" },	\
+		    { "display": "Temperatura Antigelo", "name": "Tice", "temperature": "5" },	\
+		]
+	
+	with open("setpointarray.json", "w") as outfile:
+		json.dump(SetPoints, outfile, indent=4)
+
 
 # Intestazione HTML
 print("<!DOCTYPE html>")
@@ -30,25 +45,23 @@ print("""
 
 print("""
 <h2>Inserimento temperature di Set Point</h2>
-<p><b>ATTENZIONE</b>: Accetta solo valori (numeri) interi.</p?
+<p><b>ATTENZIONE</b>: Accetta solo valori (numeri) interi.</p>
+<p>Numeri da 5 (min) a 30 (max).</p>
 <br/>
 <br/>
 """)
 
 # Start form input
 print("<form action=\"/cgi-bin/writesetpoints.py\" method=\"post\">")
+print("<table>")
 
+# Per tutta la lunghezza/voci contenute nell'array ..
 for i in range(len(SetPoints)):
-	#print(SetPoints[i]["display"])
-	#print(":")
-	#print("<input type=\"text\" name=\"")
-	#print(SetPoints[i]["name"])
-	#print("\" value=\"")
-	#print(SetPoints[i]["temperature"])
-	#print("\" required><br/>")A
-	print(SetPoints[i]["display"],":<input type=\"text\" size=\"5\" name=\"",SetPoints[i]["name"],"\" value=\"",SetPoints[i]["temperature"],"\" required><br/>", sep="")
+	# Concatenamento di variabili da SetPoins e pezzi di html
+	print("<tr><td>",SetPoints[i]["display"],":</td><td><input type=\"number\" name=\"",SetPoints[i]["name"],"\" value=\"",SetPoints[i]["temperature"],"\" min=\"5\" max=\"30\" maxlength=\"2\" size=\"2\" required><br/></td></tr>", sep="")
 
-print("<input type=\"submit\" value=\"Submit\">")
+print("<td></td><td><input type=\"submit\" value=\"Submit\"></td>")
+print("</table>")
 print("</form>")	# END form
 
 
